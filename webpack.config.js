98ed5ff6953dc23b45d72d13c207e70a80ab817d
pleahmacaka <pleahmacaka@gmail.com>
2023-02-23 03:31:26 +0900

    :star:

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require("path")
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const CopyPlugin = require("copy-webpack-plugin")
+
+module.exports = {
+    devtool: "cheap-module-source-map",
+    mode: process.env.MODE || "development",
+    experiments: {
+        topLevelAwait: true
+    },
+    entry: {
+        popup: "./src/popup/popup.ts",
+        background: "./src/background/background.ts",
+        content: "./src/content/content.ts",
+    },
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/[name]/[name].js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['.ts', '.js', '...'],
+        fallback: {}
+    },
+    plugins: [
+        new CleanWebpackPlugin({
+            cleanStaleWebpackAssets: false
+        }),
+        new CopyPlugin({
+            patterns: [
+                { from: "manifest.json", to: "manifest.json" },
+                { from: "assets", to: "assets" },
+                { from: "src/popup/popup.html", to: "popup/popup.html" },
+            ]
+        })
+    ]
+}
